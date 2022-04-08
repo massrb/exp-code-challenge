e1985971d57eea94a1b2e9bb878c0150d32455fb
Laruence Guild <laruenceguild@Laurences-MBP.fios-router.home>
2022-04-07 21:04:24 -0400

    completions count added to page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_many :todos, primary_key: :number, foreign_key: :user_number, dependent: :destroy
+
+  def completed
+    todos.where(completed: true).count
+  end  
 end
